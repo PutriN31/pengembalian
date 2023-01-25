ff69dbfd475ae2e9071ed0fa16c1cbea61edc38d
PutriN31 <113873464+PutriN31@users.noreply.github.com>
2023-01-25 21:31:39 +0700

    Add files via upload

---
+++ pengembalian-service/src/test/java/com/putri/pengembalian/service/PengembalianServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.putri.pengembalian.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PengembalianServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
